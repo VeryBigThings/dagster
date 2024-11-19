bd18bd771df3ed2abce1e87fe8857373a2901c2d
Marko Horvatic <m.horva@gmail.com>
2024-11-19 13:43:42 +0100

    chore: rename env variable names for db connection string

--- tutorial_template/assets/config.py
+++ tutorial_template/assets/config.py
@@ -3,7 +3,7 @@ import os
 config = {
     "ingestion_sources": {
         "production": {
-            "db_uri": os.environ["DB_PRODUCTION_CONN_STR"],
+            "db_uri": os.environ["DB_PRODUCTION_URI"],
             "tables": [
                 "tblBillofLadingDetail",
                 "tblBillofLadingHeader",
@@ -14,11 +14,11 @@ config = {
             "storage_path_prefix": "bronze/db/production",
         },
         "tracker": {
-            "db_uri": os.environ["DB_TRACKER_CONN_STR"],
+            "db_uri": os.environ["DB_TRACKER_URI"],
             "tables": ["tblPulperMix"],
             "storage_path_prefix": "bronze/db/tracker",
         },
     },
     "silver_path_prefix": "silver",
-    "warehouse_uri": os.environ["DB_WAREHOUSE_CONN_STR"],
+    "warehouse_uri": os.environ["DB_WAREHOUSE_URI"],
 }
