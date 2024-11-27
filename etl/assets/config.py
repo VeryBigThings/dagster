65695de319e31d7098e37a865480b7cf8d924a32
Marko Horvatic <m.horva@gmail.com>
2024-11-27 12:53:16 +0100

    feat: add config for additional database sources

--- etl/assets/config.py
+++ etl/assets/config.py
@@ -18,10 +18,25 @@ config = {
             "tables": ["tblPulperMix"],
             "storage_path_prefix": "bronze/db/tracker",
         },
+        "OBCC": {
+            "db_uri": os.environ["DB_OBCC_URI"],
+            "tables": [],
+            "storage_path_prefix": "bronze/db/OBCC",
+        },
+        "STTissueProductionQRT": {
+            "db_uri": os.environ["DB_STTISSUEPRODUCTIONQRT_URI"],
+            "tables": [],
+            "storage_path_prefix": "bronze/db/STTissueProductionQRT",
+        },
         "ctc_custom": {
             "db_uri": os.environ["DB_CTC_CUSTOM_URI"],
-            "tables": ["ctc_Chem_Data"],
+            "tables": [],
             "storage_path_prefix": "bronze/db/ctc_custom",
+        },
+        "ctc_config": {
+            "db_uri": os.environ["DB_CTC_CUSTOM_URI"],
+            "tables": [],
+            "storage_path_prefix": "bronze/db/ctc_config",
         },
     },
     "silver_path_prefix": "silver",
