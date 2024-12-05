3eb4b894693b0c6db83d13e857f1d38a5c3a6ed4
Marko Horvatic <m.horva@gmail.com>
2024-12-05 20:11:00 +0100

    feat: set can_subset=True in bronze multi_asset

--- etl/assets/stage/bronze.py
+++ etl/assets/stage/bronze.py
@@ -6,7 +6,7 @@ from dagster import (
 from ..common import get_ingestion_assets, config
 
 
-@multi_asset(outs=get_ingestion_assets())
+@multi_asset(outs=get_ingestion_assets(), can_subset=True)
 def db_ingestion():
     for source_key, source_value in config["ingestion_sources"].items():
         for table in source_value["tables"]:
