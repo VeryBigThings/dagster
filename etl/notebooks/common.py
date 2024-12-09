8766806970236ab2dce2f99ea19ac3a4678a6c25
Marko Horvatic <m.horva@gmail.com>
2024-12-09 13:48:43 +0100

    feat: add enterprise_search notebook with question examples presented as data warehouse queries

--- etl/notebooks/common.py
+++ etl/notebooks/common.py
@@ -1,6 +1,7 @@
 import os
 import boto3
 import pandas as pd
+import polars as pl
 from io import BytesIO
 from etl.assets.config import config
 
@@ -25,12 +26,24 @@ class S3Helper:
             Body=fileobj.getvalue()
         )
 
-def get_parquet_path(source_name, table_name):
+def get_parquet_path(source_name, table_name, config_key="ingestion_sources"):
     source_config = config["ingestion_sources"][source_name]
     storage_path_prefix = source_config["storage_path_prefix"]
     return os.path.join(storage_path_prefix, table_name)
 
-def load_parquet(source_name, table_name):
+def load_ingested_parquet(source_name, table_name, lib="pandas"):
     path = get_parquet_path(source_name, f'{table_name}.parquet')
     file_obj = S3Helper.download_fileobj(os.environ["AWS_BUCKET_S3"], path)
+
+    if lib == "polars":
+        return pl.read_parquet(file_obj)
+
+    return pd.read_parquet(file_obj)
+
+def load_parquet(storage_path, lib="pandas"):
+    file_obj = S3Helper.download_fileobj(os.environ["AWS_BUCKET_S3"], storage_path)
+
+    if lib == "polars":
+        return pl.read_parquet(file_obj)
+
     return pd.read_parquet(file_obj)
